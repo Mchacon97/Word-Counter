7303405f0a76d8050fb64feb296611de75cfc45e
Miguel Chacon <andrescala08@gmail.com>
2021-07-15 20:36:21 -0500

    First usable version

---
+++ Entities/cFile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entities
+{
+    public class cFile
+    {
+        public string FileName { get; set; }
+        public string Content { get; set; }
+        public List<cWord> Words { get; set; } = new List<cWord>();
+    }
+}
---
+++ Entities/cWord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities
+{
+    public class cWord
+    {
+        public string Value { get; set; }
+        public int Count { get; set; }
+        public int Length { get; set; }
+    }
+}
